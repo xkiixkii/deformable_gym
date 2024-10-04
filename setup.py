dd0266708a3544da3cfe3abea28ace7bdfa90e0d
Kaixing Xiao <xkx971128@gmail.com>
2024-10-04 16:13:57 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -16,9 +16,9 @@ if __name__ == "__main__":
         packages=["deformable_gym"],
         install_requires=[
             "pybullet",
-            "gymnasium",
+            "gymnasium>=1.0.0a2",
             "numpy>=1.23.5,<2.0.0",
             "pytransform3d",
-            "mujoco==3.1.6",
+            "mujoco>=3.2.0",
         ],
     )
